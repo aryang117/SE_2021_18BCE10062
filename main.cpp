2fb64f1b5a34ff7fd5b34884f6c397d853fa404f
Trolled00Gamer <aryu100@gmail.com>
2021-10-07 17:58:17 +0530

    added the main loop where the game will work, and added comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// board class, manages everything related to the board
 class board
 {
 private:
+    // the layout, contains the position of every pawn
     vector<vector<string>> boardLayout = vector<vector<string>>(5, vector<string>(5, "-"));
 
 public:
+    // printing the initial status of the board
     board()
     {
         for (vector<string> i : boardLayout)
@@ -18,6 +22,7 @@ public:
         }
     }
 
+    // filling the board with the players' initial position of pawns
     void updateBoardInit(unordered_map<string, vector<int>> locs, int playerID)
     {
 
@@ -29,6 +34,7 @@ public:
         }
     }
 
+    // updating the board after every move
     string updateBoardMove(unordered_map<string, vector<int>> P1locs, unordered_map<string, vector<int>> P2locs, char moveBy)
     {
         // cleaning the board
@@ -40,17 +46,7 @@ public:
             }
         }
 
-        string dedChar = "na";
-
-        // check if the a player's pawn dies
-        for (auto i : P1locs)
-        {
-            for (auto j : P2locs)
-                if (i.second[0] == j.second[0] && i.second[1] == j.second[1])
-                    dedChar = moveBy == 'A' ? 'B' + j.first : 'A' + i.first;
-        }
-
-        // updating the board
+        // filling the board with player's pawns
         for (auto i : P1locs)
         {
             boardLayout[i.second[0]][i.second[1]] = 'A' + i.first;
@@ -61,9 +57,23 @@ public:
             boardLayout[i.second[0]][i.second[1]] = 'B' + i.first;
         }
 
+        string dedChar = "na";
+
+        // check if the a player's pawn dies
+        for (auto i : P1locs)
+        {
+            for (auto j : P2locs)
+                if (i.second[0] == j.second[0] && i.second[1] == j.second[1])
+                    dedChar = moveBy == 'A' ? 'B' + j.first : 'A' + i.first;
+        }
+        cout << "-";
+
+        // updating the board
+
         return dedChar;
     }
 
+    // printing the board
     void printBoard()
     {
         for (int i = 0; i < boardLayout.size(); i++)
@@ -77,10 +87,14 @@ public:
     }
 };
 
+// player class, manages everything related to the player's pawns's status
 class player
 {
+    // the pawns
     unordered_map<string, vector<int>> pawns;
+    // id of the player
     int playerID;
+    // no of pawns left
     int pawnLeft;
 
 public:
@@ -89,6 +103,7 @@ public:
         playerID = id;
     }
 
+    //setting initial location of the pawns
     unordered_map<string, vector<int>> setInitLoc(vector<string> loc)
     {
 
@@ -104,16 +119,19 @@ public:
         return pawns;
     }
 
+    // get the location of pawns
     unordered_map<string, vector<int>> getPlayerLoc()
     {
         return pawns;
     }
 
+    // no of pawns left
     int getNoOfPawnsLeft()
     {
         return pawnLeft;
     }
 
+    // updates the pawn locations as per the move
     unordered_map<string, vector<int>> setPlayerMove(string pawnID, char move)
     {
         switch (move)
@@ -134,10 +152,10 @@ public:
         return pawns;
     }
 
+    // if the players' pawn dies, it is updated here
     void updatePawnStatus(string pawnID)
     {
         pawnLeft--;
-
         pawns.erase(pawnID);
     }
 };
@@ -147,6 +165,7 @@ int main()
     board b;
     player P1(1), P2(2);
 
+    // getting the initial locations of the pawns from the players
     vector<string> initLocation;
     string loc;
     cout << "Player 1 input:";
@@ -170,16 +189,41 @@ int main()
     b.updateBoardInit(P2.setInitLoc(initLocation), 2);
     b.printBoard();
 
-    string playerMove;
-    cin >> playerMove;
+    // keeps track of who's chance it is to make a move
+    // if it's false then A else B
+    bool whoMove = false;
 
-    string updates = b.updateBoardMove(P1.setPlayerMove(playerMove.substr(0, 2), playerMove[playerMove.size() - 1]), P2.getPlayerLoc(), 'A');
+    // the actual game, runs till either P1's or P2's number of pawns become 0
+    while (P1.getNoOfPawnsLeft() && P2.getNoOfPawnsLeft())
+    {
 
-    if (updates != "na")
-        if (updates[0] == 'A')
-            P1.updatePawnStatus(updates.substr(1, 2));
+        if (!whoMove)
+        {
 
-    cout << P1.getNoOfPawnsLeft();
+            cout << "Player A move: \t";
+            string playerMove;
+            cin >> playerMove;
+
+            string updates = b.updateBoardMove(P1.setPlayerMove(playerMove.substr(0, 2), playerMove[playerMove.size() - 1]), P2.getPlayerLoc(), 'A');
+
+            if (updates != "na")
+                P2.updatePawnStatus(updates.substr(1, 2));
+        }
+
+        else
+        {
+            cout << " Player B move: \t";
+            string playerMove;
+            cin >> playerMove;
+
+            string updates = b.updateBoardMove(P1.getPlayerLoc(), P2.setPlayerMove(playerMove.substr(0, 2), playerMove[playerMove.size() - 1]), 'B');
+
+            if (updates != "na")
+                P1.updatePawnStatus(updates.substr(1, 2));
+        }
+        b.printBoard();
+        whoMove = !whoMove;
+    }
 
     return 0;
 }
